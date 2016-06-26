3775593efb4206b9a51af224cf1d12c0c8096dcc
Gary Blackwood <gareeblackwood@gmail.com>
2016-06-26 17:32:16 +0100

    Added a basic theme.

--- create_vis/server.py
+++ create_vis/server.py
@@ -10,5 +10,10 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=app.config["PORT"])
