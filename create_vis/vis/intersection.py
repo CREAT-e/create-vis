141f7e2aac710679066e36d27e1f696f672a3cbb
Gordon Martin <gordon.hugh.martin@gmail.com>
2016-07-31 22:13:31 +0100

    more data gathering (will squash these commits ;x)

--- create_vis/vis/intersection.py
+++ create_vis/vis/intersection.py
@@ -10,6 +10,9 @@ def get_all_studies():
 
     return studies_response.json()["studies"]
 
+def count_studies_matching(studies, x, y, z):
+    return 0
+
 
 def get_valid_values(category):
     base = current_app.config["COPYRIGHT_EVIDENCE_API_URL"]
@@ -19,9 +22,10 @@ def get_valid_values(category):
     field_values = values_response.json()["values"]
     return field_values
 
-
 @intersection.route("/intersection")
 def intersections():
+    data = []
+
     category1 = request.args.get("category1")
     category2 = request.args.get("category2")
     category3 = request.args.get("category3")
@@ -35,6 +39,6 @@ def intersections():
     for cat1_val in cat1_vals:
         for cat2_val in cat2_vals:
             for cat3_val in cat3_vals:
-                a = 1 + 1
+                data_obj = {'x' : cat1_val, 'y': cat2_val, 'z': cat3_val}
 
     return jsonify({'results' : []})
